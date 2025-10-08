b802c989f3e444f6b2ed4b967665178e6c788388
aicha941 <baloubaliaicha130@gmail.com>
2025-10-08 10:26:50 +0100

    first commit

---
+++ src/interface1/INTERFACE1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interface1;
+
+/**
+ *
+ * @author fatima
+ */
+public class INTERFACE1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
